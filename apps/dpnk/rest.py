8ce1f774dd53609e2943434b75a7722fd521898f
Tomas Zigo <tomas.zigo@slovanet.sk>
2026-02-02 08:43:09 +0100

    apps/dpnk: extend REST API GET URL datareport-results/ endpoint
    By organization-coordinator URL report type regex.

--- apps/dpnk/rest.py
+++ apps/dpnk/rest.py
@@ -3839,6 +3839,7 @@ class DataReportResults(UserAttendanceMixin, APIView):
     2. Team regularity in the cities (Company/City organizator),
     3. Organization/city performance (Company/City organizator),
     4. Orgaznizations review
+    5. Organization coordinator
     """
 
     permission_classes = [permissions.IsAuthenticated]
@@ -3957,6 +3958,14 @@ class DataReportResults(UserAttendanceMixin, APIView):
             url = concat_all(
                 base_url,
             )
+        elif "organization-coordinator" == report_type:
+            base_url = (
+                settings.METABASE_DPNK_ORGANIZATION_COORDINATOR_RESULTS_DATA_REPORT_URL
+            )
+            url = concat_all(
+                base_url,
+            )
+
         return Response({"data_report_url": url})
 
 
