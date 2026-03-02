282da82acf37beb5a94bc5b8fc19fa4bdcd1219b
Tomas Zigo <tomas.zigo@slovanet.sk>
2026-03-02 10:22:42 +0100

    project/settings: add MyGLS global settings

--- project/settings/base.py
+++ project/settings/base.py
@@ -1046,3 +1046,26 @@ SIMPLE_JWT = {
 TINYMCE_SPELLCHECKER = True
 TINYMCE_COMPRESSOR = True
 TINYMCE_JS_URL = os.path.join(STATIC_URL, "tinymce/tinymce.min.js")
+
+MYGLS_API = {
+    "client_number": os.getenv("MYGLS_API_CLIENT_NUMBER"),
+    "username": os.getenv("MYGLS_API_USERNAME"),
+    "password": os.getenv("MYGLS_API_PASSWORD"),
+    "country_code": os.getenv("MYGLS_API_COUNTRY_CODE"),
+    "use_test_account": True
+    if os.getenv("MYGLS_API_USE_TEST_ACCOUNT") == "True"
+    else False,
+    "use_api": True if os.getenv("MYGLS_API_USE_API") == "True" else False,
+    "pickup_address": {
+        "City": os.getenv("MYGLS_API_PICKUP_ADDRESS_CITY"),
+        "ContactEmail": os.getenv("MYGLS_API_PICKUP_ADDRESS_CONTACT_EMAIL"),
+        "ContactName": os.getenv("MYGLS_API_PICKUP_ADDRESS_CONTACT_NAME"),
+        "ContactPhone": os.getenv("MYGLS_API_PICKUP_ADDRESS_CONTACT_PHONE"),
+        "CountryIsoCode": os.getenv("MYGLS_API_PICKUP_ADDRESS_COUNTRY_ISO_CODE"),
+        "HouseNumber": os.getenv("MYGLS_API_PICKUP_ADDRESS_HOUSE_NUMBER"),
+        "Name": os.getenv("MYGLS_API_PICKUP_ADDRESS_NAME"),
+        "Street": os.getenv("MYGLS_API_PICKUP_ADDRESS_STREET"),
+        "ZipCode": os.getenv("MYGLS_API_PICKUP_ADDRESS_ZIP_CODE"),
+        "HouseNumberInfo": os.getenv("MYGLS_API_PICKUP_ADDRESS_HOUSE_NUMBER_INFO"),
+    },
+}
