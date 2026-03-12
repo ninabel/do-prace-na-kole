b4ae5a0d68d92c323fcc08683217c3d893efcadb
Tomas Zigo <tomas.zigo@slovanet.sk>
2026-03-12 10:02:26 +0100

    project/settings: log Celery tasks logs into file

--- project/settings/base.py
+++ project/settings/base.py
@@ -609,6 +609,13 @@ LOGGING = {
             "class": "django.utils.log.AdminEmailHandler",
             "include_html": True,
         },
+        "celery": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "celery.log",
+            "formatter": "simple",
+            "maxBytes": 1024 * 1024 * 100,  # 100 Mb
+        },
     },
     "root": {
         "level": "WARNING",
@@ -639,6 +646,11 @@ LOGGING = {
         "django.security.DisallowedHost": {
             "handlers": ["mail_admins", "sentry"],
             "level": "CRITICAL",
+            "propagate": False,
+        },
+        "celery": {
+            "handlers": ["celery", "console"],
+            "level": "INFO",
             "propagate": False,
         },
     },
